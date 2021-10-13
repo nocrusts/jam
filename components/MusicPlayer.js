4b52da9d8ddf9df36dff16ee9a9ef5908c3ad393
b0kch01 <nath49ers@gmail.com>
2021-10-13 02:32:25 -0700

    More ui stuff

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -4,38 +4,60 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay, faCirclePause } from "@fortawesome/free-solid-svg-icons";
 import ReactPlayer from "react-player/youtube";
 
+import Image from "next/image";
 import { useState, useEffect } from "react";
 
 const MusicPlayer = () => {
 
+    const [url, setUrl] = useState("https://www.youtube.com/watch?v=tOZkYsrX8JI");
     const [playing, setPlay] = useState(false);
-    const [volume, setVolume] = useState(0.5);
-
+    const [volume, setVolume] = useState(0);
+    const [title, setTitle] = useState("Nothing Playing");
+    const [thumbnail, setThumbnail] = useState("/stock.png");
+    const [artist, setArtist] = useState("Unknown");
 
     useEffect(() => {
-        let volumeLocal = 50;
+        // States read values aren't updated here...(workaround)
+        let volumeLocal = parseInt(window.localStorage.getItem("volume"));
+        volumeLocal = volumeLocal > -1 ? volumeLocal : 50;
+
+        setVolume(volumeLocal / 100);
+
+        function changeVolume(percent) {
+            volumeLocal += percent;
+
+            window.localStorage.setItem("volume", volumeLocal);
+            setVolume(volumeLocal / 100);
+        }
 
         window.addEventListener("keydown", (e) => {
-
-            console.log(e.key);
-
             if (e.key == "ArrowUp" && volumeLocal < 100) {
-                volumeLocal += 5;
-                setVolume(volumeLocal / 100);
-            }
-
-            else if (e.key == "ArrowDown" && volumeLocal > 0) {
-                volumeLocal -= 5;
-                setVolume(volumeLocal / 100);
+                changeVolume(5);
+            } else if (e.key == "ArrowDown" && volumeLocal > 0) {
+                changeVolume(-5);
             }
         });
     }, []);
+
+    function getVideoDetails() {
+        fetch(`https://www.youtube.com/oembed?url=${url}&format=json`)
+            .then(res => res.json())
+            .then(json => {
+                setTitle(json.title);
+                setThumbnail(json.thumbnail_url);
+                setArtist(json.author_name);
+            })
+    }
 
     return (
         <div className={styles.player}>
 
             <div className={styles.left}>
-                Details
+                <img className={styles.thumbnail} src={thumbnail} />
+                <div className={styles.label}>
+                    <span className={styles.title}>{title}</span>
+                    <span className={styles.artist}>{artist}</span>
+                </div>
             </div>
 
             <div className={styles.controls}>
@@ -50,17 +72,18 @@ const MusicPlayer = () => {
             </div>
 
             <div className={styles.right}>
-                Adjust Volume: {Math.floor(volume * 100)}%
+                Volume: {Math.floor(volume * 100)}%
             </div>
 
             <div className={styles.backend}>
                 <ReactPlayer
-                    url="https://www.youtube.com/watch?v=GFf5qIfbGcA"
+                    url={url}
                     playing={playing}
                     controls={false}
                     playsinline={false}
                     pip={false}
                     volume={volume}
+                    onReady={getVideoDetails}
                 />
             </div>
         </div >
