18ca6ddd0ca616633dbe9af197544b1640939bf4
b0kch01 <nath49ers@gmail.com>
2021-10-13 17:21:16 -0700

    audio player / UI is much better

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -4,14 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay, faCirclePause } from "@fortawesome/free-solid-svg-icons";
 import ReactPlayer from "react-player/youtube";
 
-import Image from "next/image";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 
 const MusicPlayer = () => {
-
     const [url, setUrl] = useState("https://www.youtube.com/watch?v=tOZkYsrX8JI");
     const [playing, setPlay] = useState(false);
     const [played, setPlayed] = useState(0);
+
     const [volume, setVolume] = useState(0);
     const [seeking, setSeeking] = useState(false);
 
@@ -21,28 +20,36 @@ const MusicPlayer = () => {
 
     const ref = useRef(null);
 
+    const adjustVolume = (percent) => {
+        setVolume(preVolume => Math.min(100, Math.max(0, preVolume + percent)));
+        window.localStorage.getItem("volume")
+    };
+
     useEffect(() => {
-        // States read values aren't updated here...(workaround)
-        let volumeLocal = parseInt(window.localStorage.getItem("volume"));
-        volumeLocal = volumeLocal > -1 ? volumeLocal : 50;
+        setVolume(Number(window.localStorage.getItem("volume") || "50"));
 
-        setVolume(volumeLocal / 100);
-
-        function changeVolume(percent) {
-            volumeLocal += percent;
-
-            window.localStorage.setItem("volume", volumeLocal);
-            setVolume(volumeLocal / 100);
-        }
-
-        window.addEventListener("keydown", (e) => {
-            if (e.key == "ArrowUp" && volumeLocal < 100) {
-                changeVolume(5);
-            } else if (e.key == "ArrowDown" && volumeLocal > 0) {
-                changeVolume(-5);
+        const handleKeyPress = (e) => {
+            if (e.key == "ArrowUp") {
+                adjustVolume(5);
             }
-        });
+
+            if (e.key == "ArrowDown") {
+                adjustVolume(-5);
+            }
+
+            if (e.key == " ") {
+                togglePlay();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyPress);
+
+        return () => window.removeEventListener("keydown", handleKeyPress);
     }, []);
+
+    function togglePlay(e) {
+        setPlay(currentState => !currentState);
+    }
 
     function handleSeekMouseDown() {
         setSeeking(true);
@@ -63,6 +70,13 @@ const MusicPlayer = () => {
         }
     }
 
+    function handleVolumeChange(e) {
+        let newVolume = parseInt(e.target.value);
+
+        setVolume(newVolume);
+        window.localStorage.setItem("volume", newVolume);
+    }
+
     function getVideoDetails() {
         fetch(`https://www.youtube.com/oembed?url=${url}&format=json`)
             .then(res => res.json())
@@ -77,7 +91,7 @@ const MusicPlayer = () => {
         <div className={styles.player}>
 
             <div className={styles.left}>
-                <img className={styles.thumbnail} src={thumbnail} />
+                <img className={styles.thumbnail} src={thumbnail} alt="album" />
                 <div className={styles.label}>
                     <span className={styles.title}>{title}</span>
                     <span className={styles.artist}>{artist}</span>
@@ -87,7 +101,8 @@ const MusicPlayer = () => {
             <div className={styles.controls}>
                 <button
                     className={styles.playPause}
-                    onClick={function () { setPlay(!playing) }}
+                    onClick={togglePlay}
+                    onKeyDown={(e) => e.preventDefault()}
                 >
                     <FontAwesomeIcon
                         icon={playing ? faCirclePause : faCirclePlay}
@@ -96,16 +111,29 @@ const MusicPlayer = () => {
                 </button>
                 <input
                     className={styles.seek}
-                    type='range' min={0} max={0.999999} step='any'
+                    type="range" min={0} max={0.999999} step="any"
                     value={played}
                     onMouseDown={handleSeekMouseDown}
                     onChange={handleSeekChange}
                     onMouseUp={handleSeekMouseUp}
+                    onKeyDown={event => event.preventDefault()}
+                    style={{
+                        background: 'linear-gradient(to right, rgb(var(--accent)) 0%, rgb(var(--accent)) ' + played * 100 + '%, rgba(255, 255, 255, 0.1) ' + played * 100 + '%, transparent 140%)'
+                    }}
                 />
             </div>
 
             <div className={styles.right}>
-                Volume: {Math.floor(volume * 100)}%
+                <input
+                    className={styles.volume}
+                    type="range" min={0} max={100} step="any"
+                    value={volume}
+                    onChange={handleVolumeChange}
+                    onKeyDown={event => event.preventDefault()}
+                    style={{
+                        background: 'linear-gradient(to right, rgb(var(--accent)) 0%, rgb(var(--accent)) ' + volume + '%, rgba(255, 255, 255, 0.1) ' + volume + '%, transparent 140%)'
+                    }}
+                />
             </div>
 
             <div className={styles.backend}>
@@ -116,9 +144,21 @@ const MusicPlayer = () => {
                     controls={false}
                     playsinline={false}
                     pip={false}
-                    volume={volume}
+                    volume={volume / 100}
                     onReady={getVideoDetails}
                     onProgress={handleProgress}
+                    config={
+                        {
+                            youtube: {
+                                playerVars: {
+                                    autoplay: 0,
+                                    modestbranding: 1,
+                                    showinfo: 0,
+                                    disablekb: 1
+                                }
+                            }
+                        }
+                    }
                 />
             </div>
         </div >
